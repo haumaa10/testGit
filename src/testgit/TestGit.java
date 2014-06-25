063b1f9890535cef5d80703b41b6daac88d32184
martin <martin@martin-PC>
2014-06-25 19:54:05 +0200

    thing

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -17,7 +17,7 @@ public class TestGit {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("1,2,3");
+        System.out.println("1,2,3,5");
     }
     
 }
